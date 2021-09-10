aebfb524aeff0508bc5dfbd7ce1c62233bb95b94
Your Name <jpark1917@gmail.comecho>
2021-09-10 08:25:46 -0700

    updated markdowns

--- Chat_Transcript2021/Short_enroll.R
+++ Chat_Transcript2021/Short_enroll.R
@@ -56,8 +56,59 @@ short_enroll$class_standing = recode_factor(short_enroll$Class_Standing__c,
 # merged_data <- merge(x=short_enroll, y=chat2021, by.x="Contact__c", by.y="ContactId", x.all=FALSE, y.all=FALSE) 
 # 
 # inner_enroll <- inner_join(chat2021, enrollcontact, by = "ContactId") #created a few duplicate rows
-# 
-left_enroll <- left_join(chat2021, short_enroll, by = "new_ID") # created a ton of duplicates
+chattran <- read.csv("chattran_did.csv", header = TRUE, na.strings=c("","NA"))
+
+# recode developer name
+chattran$DeveloperName = recode_factor(chattran$LiveChatDeployment.DeveloperName, 
+                                       "X24_7_Chat" = "24/7",
+                                       "University_Services_Chat" = "Other",
+                                       "Think_Tank_Chat" = "Other",
+                                       "Study_Abroad_Chat" = "Other",
+                                       "SOS_Chat" = "SOS",
+                                       "SECD_Chat" = "Other",
+                                       "Registrar_Chat" = "Registrar",
+                                       "Psych_Dept_Chat" = "Other",
+                                       "OSFA_Chat" = "OSFA", 
+                                       "LifeLab_Chat" = "Other",
+                                       "College_of_Engineering_Chat" = "Other")
+
+## add days of the week for chat 2021
+date_value3 = chattran$CreatedDate
+day_of_the_week3 = date(date_value3) %>%
+  wday()
+# day_of_the_week
+
+chattran = dplyr::mutate(chattran, day_of_the_week3)
+chattran$day_of_the_week <- recode_factor(chattran$day_of_the_week, 
+                                          "1" = "Sunday", 
+                                          "2" = "Monday",
+                                          "3" = "Tuesday",
+                                          "4" = "Wednesday",
+                                          "5" = "Thursday",
+                                          "6" = "Friday",
+                                          "7" = "Saturday")
+
+# check that the date time is in the right time zone
+# ymd_hms("2020-10-08T18:06:08.000Z")
+# table(chat2021$CreatedDate)
+Date_Time3 = ymd_hms(chattran$CreatedDate)
+# head(Date_Time) #2020-07-02 21:56:42 UTC
+chattran <- dplyr::mutate(chattran, Date_Time3)
+
+# change to AZ time 
+t3 <- as.POSIXct(Date_Time3, tz = "GMT")
+attributes(t3)$tzone
+AZ_time3 <- lubridate::with_tz(t3, "MST")
+
+chattran <- dplyr::mutate(chattran, AZ_time3)
+
+# convert time to decimals 
+tm3.dechr <- hour(chattran$AZ_time) + minute(chattran$AZ_time)/60 + second(chattran$AZ_time)/3600
+# tm1.dechr
+
+chattran <- dplyr::mutate(chattran, tm3.dechr) 
+# left join to preserve chat data
+left_enroll <- left_join(chattran, short_enroll, by = "new_ID") # created a ton of duplicates
 # 
 # semi_enroll <- semi_join(chat2021, enrollcontact, by = "ContactId") # created a ton of duplicates
 # 
@@ -81,27 +132,132 @@ write.csv(chat_gpa, "chat_gpa.csv", row.names = FALSE)
 
 # degree_chat <- chat_gpa %>% 
 #   drop_na(Degree__c) #too many cuts
-
+#
 
 
 # graphs distinguishing students
-left_graph <- chat_gpa %>%
+left_graph <- left_enroll %>%
   # mutate(Q7 = fct_infreq(Q7)) %>%
   drop_na(Students) %>% 
   ggplot(aes(x = DeveloperName, fill = Students, response = Students)) +
   geom_bar() +
-  # coord_flip() +
+  coord_flip() +
   labs(x = "", y = "")
 
-left_graph <- left_graph + labs(title = "",
+left_graph <- left_graph + labs(title = "Chat Usage by Students",
                                 subtitle = "undergraduate and graduate students")
 
 print(left_graph)
 
 ## count up the NA's
-sum(is.na(chat_gpa$Academic_Standing__c)) # 4224
+sum(is.na(chat_gpa$Academic_Standing__c)) # 4570
+
+# make tables
+library(dplyr)
+library(knitr)
+library(DT)
+library(xtable)
+library(tibble)
+
+# exploring the data
+# colMeans(chat_gpa[sapply(chat_gpa, is.numeric)])
+# sapply(chat_gpa, class)
+
+left_enroll_table <- left_enroll %>% 
+  select(Abandoned, ChatDuration, MaxResponseTimeOperator, MaxResponseTimeVisitor, 
+         OperatorMessageCount,WaitTime, tm3.dechr, Cumulative_GPA__c)
+
+colMeans(left_enroll_table, na.rm=TRUE)
+# for summary tables
+
+left_enroll_table <- left_enroll %>% 
+  select(Abandoned, ChatDuration, MaxResponseTimeOperator, MaxResponseTimeVisitor, 
+         OperatorMessageCount,WaitTime, tm3.dechr, Cumulative_GPA__c)
+
+d.summary.extended <-  left_enroll_table <- left_enroll %>% 
+  select(Abandoned, ChatDuration, MaxResponseTimeOperator, MaxResponseTimeVisitor, 
+         OperatorMessageCount,WaitTime, tm3.dechr, Cumulative_GPA__c) %>% 
+  psych::describe(quant=c(.25,.75)) %>%
+  as_tibble() %>%
+  rownames_to_column() %>%
+  print()
+
+dim(left_enroll_table)
+
+# install.packages("stargazer")
+library(stargazer)
+# cols <- c('Abandoned', 'ChatDuration', 'MaxResponseTimeOperator', 'MaxResponseTimeVisitor', 
+#           'OperatorMessageCount', 'WaitTime', 'tm3.dechr', 'Cumulative_GPA__c')
+# stargazer(
+#   left_enroll[, cols], type = "text", 
+#   summary.stat = c("min", "p25", "median", "p75", "max", "median", "sd")
+# )
 
 # try new graphs with the data
 # scatter plot
 ggplot(chat_gpa, aes(x=WaitTime/60, y=ChatDuration/60, color=class_standing)) + 
   geom_point(size=6) 
+
+# pie charts with plotly
+# annotations
+# a <- list(
+#   text = "SUBPLOT TITLE A",
+#   xref = "paper",
+#   yref = "paper",
+#   yanchor = "bottom",
+#   xanchor = "center",
+#   align = "center",
+#   x = 0.5,
+#   y = 1,
+#   showarrow = FALSE
+# )
+# 
+# b <- list(
+#   text = "SUBPLOT TITLE B",
+#   xref = "paper",
+#   yref = "paper",
+#   yanchor = "bottom",
+#   xanchor = "center",
+#   align = "center",
+#   x = 0.5,
+#   y = 1,
+#   showarrow = FALSE
+# )
+
+fig <- plot_ly()
+fig <- fig %>% add_pie(data = count(short_enroll, class_standing), labels = ~class_standing, 
+                       values = ~n, name = "enrollment", domain = list(x = c(0, 0.4), 
+                                                                       y = c(0.4, 1)))
+fig <- fig %>% add_pie(data = count(chat_gpa, class_standing), labels = ~class_standing, 
+                       values = ~n, name = "chat", domain = list(x = c(0.6, 1), y = c(0.4, 1)))
+
+fig <- fig %>% layout(title = "Class Standing by Enrollment and Chat Usage", showlegend = T,
+                      grid=list(rows=0, columns=1),
+                      xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                      yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                      legend = list(y = 0.5),
+                      annotations = list(x = c(.08, .62, .08, .62),
+                                         # y = c(.78, .78, .22, .22),
+                                         text = c("Enrollment","Chat Usage"),
+                                         # xref = "papper",
+                                         yref = "papper",
+                                         showarrow = F))
+
+fig
+
+# ##### NLP
+# library(gutenbergr)
+# library(tidytext)
+# library(stringr) 
+# library(tidyr)   
+# # install.packages("wordcloud")
+# library(wordcloud)
+# library(hunspell)
+# library(SnowballC)
+# library(xtable)
+# library(knitr)
+# library(kableExtra)
+# 
+# # preprocessing data
+# cleaned_text <- chat2021 %>%
+#   filter(str_detect(text_review, "^[^>]+[A-Za-z\\d]") | text_review !="") 
--- live_agent.R
+++ live_agent.R
@@ -43,11 +43,10 @@ AZ_time2 <- lubridate::with_tz(t2, "MST")
 # head(AZ_time)
 LiveAgent2 <- dplyr::mutate(LiveAgent2, AZ_time2)
 
-# convert to decimal time
+# convert to decimal time instead of hour_created
 tm2.dechr <- hour(LiveAgent2$AZ_time2) + minute(LiveAgent2$AZ_time2)/60 + second(LiveAgent2$AZ_time2)/3600
 # tm2.dechr
 #### updated dataset is LiveAgent2
-summary(LiveAgent2$hour_created)
 summary(LiveAgent2$ChatReqTimedOut) # max is about 1010 for time maxed out
 
 tibble(LiveAgent2$TimeAtCapacity, LiveAgent2$TimeIdle, LiveAgent2$TimeInAwayStatus)
@@ -56,23 +55,30 @@ tibble(LiveAgent2$TimeAtCapacity, LiveAgent2$TimeIdle, LiveAgent2$TimeInAwayStat
 
 # explore the data
 glimpse(LiveAgent2)
-# finding means
-mean(LiveAgent2$TimeAtCapacity)
+# finding means of Time variables
+# mean(LiveAgent2$TimeAtCapacity)
+# 
+# LiveAgent2 %>% 
+#   dplyr::summarize(TimeAtCap = mean(TimeAtCapacity, na.rm=TRUE)/60) 
+# 
+# LiveAgent2 %>% 
+#   dplyr::summarize(idle = mean(TimeIdle, na.rm=TRUE)/60)  # seems to have a huge outlier
+# 
+# LiveAgent2 %>% 
+#   dplyr::summarize(InAway = mean(TimeInAwayStatus, na.rm=TRUE)/60)
+# 
+# LiveAgent2 %>% 
+#   dplyr::summarize(in_chats = mean(TimeInChats, na.rm=TRUE)/60)
+# 
+# LiveAgent2 %>% 
+#   dplyr::summarize(online = mean(TimeInOnlineStatus, na.rm=TRUE)/60)
 
-LiveAgent2 %>% 
-  dplyr::summarize(TimeAtCap = mean(TimeAtCapacity, na.rm=TRUE)/60) 
+#https://www.statology.org/calculate-mean-multiple-columns-in-r/
+LiveA_table <- LiveAgent2 %>% 
+  select(starts_with("Time"))
 
-LiveAgent2 %>% 
-  dplyr::summarize(idle = mean(TimeIdle, na.rm=TRUE)/60)  # seems to have a huge outlier
+colMeans(LiveA_table, na.rm = TRUE)
 
-LiveAgent2 %>% 
-  dplyr::summarize(InAway = mean(TimeInAwayStatus, na.rm=TRUE)/60)
-
-LiveAgent2 %>% 
-  dplyr::summarize(in_chats = mean(TimeInChats, na.rm=TRUE)/60)
-
-LiveAgent2 %>% 
-  dplyr::summarize(online = mean(TimeInOnlineStatus, na.rm=TRUE)/60)
 
 # also examine the ChatReq
 ChatReq <- LiveAgent2 %>% 
@@ -162,16 +168,16 @@ LiveAgent2_all <- (Agent_ToD + Agent_ToD1) / (Agent_ToD2 + Agent_ToD3) +    # Cr
 LiveAgent2_all 
 
 ### box plot of login 
-Date_Time3 = ymd_hms(LiveAgent2$LoginTime)
-
-t3 <- as.POSIXct(Date_Time3, tz = "GMT")
-attributes(t2)$tzone
-AZ_time3 <- lubridate::with_tz(t3, "MST")
-
-LiveAgent2 <- dplyr::mutate(LiveAgent2, AZ_time3)
+# Date_Time3 = ymd_hms(LiveAgent2$LoginTime)
+# 
+# t3 <- as.POSIXct(Date_Time3, tz = "GMT")
+# attributes(t2)$tzone
+# AZ_time3 <- lubridate::with_tz(t3, "MST")
+# 
+# LiveAgent2 <- dplyr::mutate(LiveAgent2, AZ_time3)
 
 # convert to decimal time
-login <- hour(LiveAgent2$AZ_time3) + minute(LiveAgent2$AZ_time3)/60 + second(LiveAgent2$AZ_time3)/3600
+login <- hour(LiveAgent2$AZ_time2) + minute(LiveAgent2$AZ_time2)/60 + second(LiveAgent2$AZ_time2)/3600
 
 Agent_login <- ggplot(LiveAgent2, aes(x = login, 
                                      y = tm2.dechr, fill = ChatReqAssigned)) +
@@ -211,71 +217,71 @@ print(Agent_hour)
 # 
 # head(LiveAgent2_time)
 
-plot <- ggplot(LiveAgent2_time, aes(x=Hour, y=value, group = Id)) +
-  geom_line() + labs(y= "Deaths", x = "Day")
-plot + ggtitle("")
-  +geom_point()
-
-print(plot)
+# plot <- ggplot(LiveAgent2_time, aes(x=Hour, y=value, group = Id)) +
+#   geom_line() + labs(y= "Deaths", x = "Day")
+# plot + ggtitle("")
+#   +geom_point()
+# 
+# print(plot)
 
 ##### More useful graphs that show both hour created and login time
-library(quantmod)
+# library(quantmod)
+# 
+# LiveAgent2_Time_plot <- ggplot(LiveAgent2, aes(x=ChatReqEngaged, y=tm2.dechr)) + 
+#   geom_point(aes(y = tm2.dechr, colour = "tm2.dechr")) +
+#   # geom_smooth() +
+#   geom_point(aes(y = login, colour = "login")) +
+#   # geom_smooth()+
+#   ylab("Hour") +
+#   xlab("Number of Chat Requests Engaged") +
+#   scale_color_discrete(name="Time", labels=c("Chat Created", "Agent Login Time"))
+# 
+# LiveAgent2_Time_plot <- LiveAgent2_Time_plot + labs(title = "Time of Chat Requests Engaged based on Time of Day", 
+#                                                     subtitle="full data", fill = "Time") +
+#   scale_y_continuous(breaks=seq(0,24,2)) +
+#   scale_x_continuous()
+# 
+# LiveAgent2_Time_plot
 
-LiveAgent2_Time_plot <- ggplot(LiveAgent2, aes(x=ChatReqEngaged, y=tm2.dechr)) + 
-  geom_point(aes(y = tm2.dechr, colour = "tm2.dechr")) +
-  # geom_smooth() +
-  geom_point(aes(y = login, colour = "login")) +
-  # geom_smooth()+
-  ylab("Hour") +
-  xlab("Number of Chat Requests Engaged") +
-  scale_color_discrete(name="Time", labels=c("Chat Created", "Agent Login Time"))
-
-LiveAgent2_Time_plot <- LiveAgent2_Time_plot + labs(title = "Time of Chat Requests Engaged based on Time of Day", 
-                                                    subtitle="full data", fill = "Time") +
-  scale_y_continuous(breaks=seq(0,24,2)) +
-  scale_x_continuous()
-
-LiveAgent2_Time_plot
-
-## chat req timed out
-LiveAgent2_Time_out <- ggplot(LiveAgent2, aes(x=ChatReqTimedOut, y=tm2.dechr)) + 
-  geom_point(aes(y = tm2.dechr, colour = "tm2.dechr")) +
-  # geom_smooth() +
-  geom_point(aes(y = login, colour = "login")) +
-  # geom_smooth()+
-  ylab("Hour") +
-  xlab("Chat Requests Timed Out") +
-  scale_color_discrete(name="Time", labels=c("Chat Created", "Agent Login Time"))
-
-LiveAgent2_Time_out <- LiveAgent2_Time_out + labs(title = "Time of Chat Requests Timed Out based on Time of Day", 
-                                                    subtitle="full data", fill = "Time") +
-  scale_y_continuous(breaks=seq(0,24,2)) +
-  scale_x_continuous()
-
-LiveAgent2_Time_out
+# ## chat req timed out
+# LiveAgent2_Time_out <- ggplot(LiveAgent2, aes(x=ChatReqTimedOut, y=tm2.dechr)) + 
+#   geom_point(aes(y = tm2.dechr, colour = "tm2.dechr")) +
+#   # geom_smooth() +
+#   geom_point(aes(y = login, colour = "login")) +
+#   # geom_smooth()+
+#   ylab("Hour") +
+#   xlab("Chat Requests Timed Out") +
+#   scale_color_discrete(name="Time", labels=c("Chat Created", "Agent Login Time"))
+# 
+# LiveAgent2_Time_out <- LiveAgent2_Time_out + labs(title = "Time of Chat Requests Timed Out based on Time of Day", 
+#                                                     subtitle="full data", fill = "Time") +
+#   scale_y_continuous(breaks=seq(0,24,2)) +
+#   scale_x_continuous()
+# 
+# LiveAgent2_Time_out
 ### new attempt at a combined graph
 # plot(LiveAgent2$ChatReqEngaged, LiveAgent2$hour_created, col="blue", pch="o", lty=1)
 # points(LiveAgent2$ChatReqEngaged,LiveAgent2$login, col="red", pch="*")
 # lines(LiveAgent2$ChatReqEngaged,LiveAgent2$login, col="red",lty=2)
-
-# live agent time at capacity
-LiveAgent2_Cap <- ggplot(LiveAgent2, aes(x=TimeAtCapacity, y=hour_created)) + 
-  geom_point(aes(y = hour_created, colour = "hour_created")) +
-  geom_point(aes(y = login, colour = "login")) +
-  ylab("Hour") +
-  xlab("Time at Capacity") + 
-  scale_color_discrete(name="Time", labels=c("Chat Created", "Agent Login Time"))
-
-LiveAgent2_Cap <- LiveAgent2_Cap + labs(title = "Time at Capacity based on Time of Day", 
-                                        subtitle = "full data") +
-  scale_y_continuous(breaks=seq(0,24,2)) 
-  # scale_color_discrete(name = "Legend")
-
-LiveAgent2_Cap
+# 
+# # live agent time at capacity
+# LiveAgent2_Cap <- ggplot(LiveAgent2, aes(x=TimeAtCapacity, y=hour_created)) + 
+#   geom_point(aes(y = hour_created, colour = "hour_created")) +
+#   geom_point(aes(y = login, colour = "login")) +
+#   ylab("Hour") +
+#   xlab("Time at Capacity") + 
+#   scale_color_discrete(name="Time", labels=c("Chat Created", "Agent Login Time"))
+# 
+# LiveAgent2_Cap <- LiveAgent2_Cap + labs(title = "Time at Capacity based on Time of Day", 
+#                                         subtitle = "full data") +
+#   scale_y_continuous(breaks=seq(0,24,2)) 
+#   # scale_color_discrete(name = "Legend")
+# 
+# LiveAgent2_Cap
 
 # live agent time idle
-LiveAgent2_Idle <- ggplot(LiveAgent2, aes(x=TimeIdle/60, y=hour_created)) + 
-  geom_point(aes(y = hour_created, colour = "hour_created")) +
+LiveAgent2_Idle <- ggplot(LiveAgent2, aes(x=TimeIdle/60, y=tm2.dechr)) + 
+  geom_point(aes(y = tm2.dechr, colour = "tm2.dechr")) +
   geom_point(aes(y = login, colour = "login")) +
   ylab("Hour") +
   xlab("Idle Time (minutes)") + 
@@ -284,27 +290,28 @@ LiveAgent2_Idle <- ggplot(LiveAgent2, aes(x=TimeIdle/60, y=hour_created)) +
 LiveAgent2_Idle <- LiveAgent2_Idle + labs(title = "Idle Time based on Time of Day", 
                                           subtitle = "full data") +
   scale_y_continuous(breaks=seq(0,24,2)) 
+print(LiveAgent2_Idle)
 # scale_color_discrete(name = "Legend")
 
-LiveAgent2_Idle
-### select data columns first
-LiveAgent2_time <- LiveAgent2 %>% 
-  dplyr:: select(c("hour_created","login", "ChatReqEngaged"))
-
-ggp <- ggplot(LiveAgent2_time, aes(x=ChatReqEngaged, y=login, col = hour_created)) +             # Create ggplot2 plot
-  geom_line()
-ggp # I don't know how to interpret this 
-
-### plot for time idle and chat req timed out
-mod8plot <- ggplot(LiveAgent2, aes(x= TimeIdle/60, y = ChatReqTimedOut)) +
-  geom_line() +
-  # geom_smooth() +
-  stat_smooth(method = "loess", se = FALSE) +
-labs(x="Idle Time (minutes)", y="Chat Requests Timed Out") 
-
-mod8plot <- mod8plot + labs(title = "Chat Requests Timed Out by Idle Time", 
-                            subtitle = "full data") 
-# scale_color_discrete(name = "Live Chat Developer")
-
-mod8plot
-
+# LiveAgent2_Idle
+# ### select data columns first
+# LiveAgent2_time <- LiveAgent2 %>% 
+#   dplyr:: select(c("hour_created","login", "ChatReqEngaged"))
+# 
+# ggp <- ggplot(LiveAgent2_time, aes(x=ChatReqEngaged, y=login, col = hour_created)) +             # Create ggplot2 plot
+#   geom_line()
+# ggp # I don't know how to interpret this 
+# 
+# ### plot for time idle and chat req timed out
+# mod8plot <- ggplot(LiveAgent2, aes(x= TimeIdle/60, y = ChatReqTimedOut)) +
+#   geom_line() +
+#   # geom_smooth() +
+#   stat_smooth(method = "loess", se = FALSE) +
+# labs(x="Idle Time (minutes)", y="Chat Requests Timed Out") 
+# 
+# mod8plot <- mod8plot + labs(title = "Chat Requests Timed Out by Idle Time", 
+#                             subtitle = "full data") 
+# # scale_color_discrete(name = "Live Chat Developer")
+# 
+# mod8plot
+# 
